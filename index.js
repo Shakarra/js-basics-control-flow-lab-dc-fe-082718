9dd5df6203c664ddb50a51bd5e7ca8d9e9bdb869
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-15 23:55:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if (someValue < 400) {
+    result = 'This one is on me!';
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500){
+    result = 'No can do.';
+  }
+  return result;
+}
+
+function ternaryCheckCity (NYC) {
+ return (NYC) ? "Ok, sounds good.":"No go.";
+}
+  
+  /*let tip;
+  switch(switchOnCharmFromTip) {
+    case "generous": 
+    generous = "Thank you so much.";
+    break;
+    
+    case "not generous": 
+    not generous: "Thank you.";
+    break;
+  }*/
